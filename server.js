7648d28ddacf8dec5a4a5c939119dece0288ab5c
Alexander Hill <me@alxhill.com>
2015-02-14 23:42:38 +0000

    Added api methods

--- server.js
+++ server.js
@@ -11,6 +11,32 @@ app.get('/client.js', function(req, res) {
     res.sendFile(__dirname + "/client.js");
 });
 
+app.post('/start/:players', function(req, res) {
+    var players = parseInt(req.params.players);
+
+    if (players < 5 || players > 10) {
+        res.status(400).send("must be between 5 and 10 players");
+        return;
+    }
+
+    gameState = {
+        playerCount: players,
+        players: [],
+        started: false
+    };
+
+    res.send("success");
+});
+
+app.post('/addplayer/:name', function(req, res) {
+    if (gameState.players.length >= playerCount)
+        return res.status(400).send("all players have already joined");
+
+    gameState.players.push(req.params.name);
+    if (gameState.players.length == playerCount)
+        gameState.started = true;
+});
+
 var server = app.listen(8080, function() {
     var host = server.address().address;
     var port = server.address().port;

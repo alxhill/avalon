e6d4f5631f637da9a1b89a1219b3f011c8420268
James Burnside <mail@jamesburnside.com>
2015-02-15 00:22:49 +0000

    alert error

--- client.js
+++ client.js
@@ -1,1 +1,15 @@
-alert("hey guyz xoxo");
+$(document).ready(function() {
+	console.log("edsf");
+$( "#startGame" ).submit(function( event ) {
+ 	alert("dsf");
+  // Stop form from submitting normally
+  event.preventDefault();
+ 
+  // Get some values from elements on the page:
+  var $form = $( this ),
+    term = ($('#noUsers').val());
+  // Send the data using post
+  $.post( "/start/" + term, function(done) {alert(done)});
+ 
+});
+});
